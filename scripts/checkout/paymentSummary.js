d4d1289ea9e59b9e982a48ff7b1a25258c0fb5bc
usemnm <116705685+usemnm@users.noreply.github.com>
2024-06-19 19:30:56 +0800

    Update paymentSummary.js

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -73,20 +73,25 @@ export function renderPaymentSummary() {
   document.querySelector('.js-return-to-home-lin')
     .innerHTML=`Item ${productQuantity}`;
 
-    paypal.Buttons({
-      createOrder: function(data, actions) {
-        return actions.order.create({
-          purchase_units: [{
-            amount: {
-              value:totalCents1 // Can reference variable or function for dynamic value
-            }
-          }]
-        });
-      },
-      onApprove: function(data, actions) {
-        return actions.order.capture().then(function(details) {
-          alert('Transaction completed by ' + details.payer.name.given_name);
-        });
-      }
-    }).render('#paypal-button-container');
+    function updatePayPalButton(amount) {
+      document.getElementById('paypal-button-container').innerHTML = '';
+     paypal.Buttons({
+         createOrder: function(data, actions) {
+             return actions.order.create({
+                 purchase_units: [{
+                     amount: {
+                         value: amount // Dynamically set the amount
+                     }
+                 }]
+             });
+         },
+         onApprove: function(data, actions) {
+             return actions.order.capture().then(function(details) {
+                 alert('Transaction completed by ' + details.payer.name.given_name);
+             });
+         }
+     }).render('#paypal-button-container');
+ }
+
+ updatePayPalButton(totalCents1);
 }
